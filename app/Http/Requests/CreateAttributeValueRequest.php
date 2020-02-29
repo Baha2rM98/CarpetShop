f0d770122e0a64ccc0f42f95a89e84ad8b9f3a65
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-01 03:13:25 +0330

    This class created to handle validation on creating and editing attribute values

---
+++ app/Http/Requests/CreateAttributeValueRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateAttributeValueRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'max:50', 'min:2'],
+            'attributes_group_id' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'مقدار ویژگی نمیتواند خالی باشد!',
+            'title.max' => 'مقدار ویژگی نمیتواند بیشتر از 50 کاراکتر باشد!',
+            'title.min' => 'مقدار ویژگی نمیتواند کمتر از 2 کاراکتر باشد!',
+            'attributes_group_id.required' => 'ویژگی نمیتواند خالی باشد!',
+        ];
+    }
+}
