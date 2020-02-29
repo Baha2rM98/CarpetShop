2fb3b7198e4bb11eea6f81e3f8b99b3e605517d9
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-01 03:17:48 +0330

    A comment added

--- app/AttributeGroup.php
+++ app/AttributeGroup.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 /**
  * @property mixed title
@@ -11,4 +12,13 @@ use Illuminate\Database\Eloquent\Model;
 class AttributeGroup extends Model
 {
     protected $table = 'attributes_group';
+
+    /**
+     * Returns a one-to-many relationship with AttributeValue
+     * @return Relation
+     */
+    public function attributesValue()
+    {
+        return $this->hasMany(AttributeGroup::class, 'attribute_group_id');
+    }
 }
