8ea699945c598e2c65b3d09c835231228c1ce421
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-02-29 04:25:16 +0330

    A recursive relation added

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
+/**
+ * @property mixed parent_id
+ * @property mixed name
+ * @property mixed meta_title
+ * @property mixed meta_desc
+ * @property mixed meta_keywords
+ */
 class Category extends Model
 {
-    //
+    /**
+     * Returns children of each category recursively
+     * @return Relation
+     */
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id')->with('children');
+    }
 }
