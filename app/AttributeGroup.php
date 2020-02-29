68df0d78a45ed731099b3b2a7f5b15f00f53a28b
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-02-29 17:46:02 +0330

    Model attribute group created

---
+++ app/AttributeGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property mixed title
+ * @property mixed type
+ */
+class AttributeGroup extends Model
+{
+    protected $table = 'attributes_group';
+}
