97b88c4471c05994faa256e2530275aac97eeec6
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-02-29 21:33:38 +0330

    Attribute value table created

---
+++ database/migrations/2020_02_29_171018_create_attributes_value_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesValueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes_value', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->unsignedInteger('attribute_group_id');
+            $table->foreign('attribute_group_id')->references('id')->on('attributes_group')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes_value');
+    }
+}
