ff95a158f5694c1c870676127504669242e173f9
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-02 02:34:57 +0330

    Attribute value model completed, relation with AttributeGroup model added

--- app/AttributeValue.php
+++ app/AttributeValue.php
@@ -3,8 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
+/**
+ * @property mixed title
+ * @property mixed attribute_group_id
+ */
 class AttributeValue extends Model
 {
-    //
+    protected $table = 'attributes_value';
+
+    /**
+     * Returns a one-to-many relationship with AttributeGroup
+     * @return Relation
+     */
+    public function attributeGroup()
+    {
+        return $this->belongsTo(AttributeGroup::class, 'attribute_group_id');
+    }
 }
