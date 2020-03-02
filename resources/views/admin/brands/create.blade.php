1a1467249e66afaa60cc4fcaf3c46e47e956818d
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-03 03:11:36 +0330

    Almost done...

--- resources/views/admin/brands/create.blade.php
+++ resources/views/admin/brands/create.blade.php
@@ -15,34 +15,40 @@
                     <ul>
                         <div class="alert alert-warning">
                             @foreach($errors->all() as $error)
-                                {{$error}}
+                                <ul>{{$error}}</ul>
                             @endforeach
                         </div>
                     </ul>
                 @endif
-                <div class="row">
-                    <div class="col-md-6 col-md-offset-3">
-                        <form method="post" action="/administrator/brands">
-                            @csrf
-                            <div class="form-group">
-                                <label for="name">نام</label>
-                                <input type="text" name="title" class="form-control"
-                                       placeholder="نام برند را وارد کنید...">
-                            </div>
-                            <div class="form-group">
-                                <label for="name">توضیحات</label>
-                                <textarea type="text" name="description" class="form-control"
-                                          placeholder="توضیحات برند را وارد کنید..."></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label for="photo">تصویر</label>
-                                <div id="photo" class="dropzone"></div>
-                            </div>
+                    {{--                    @if(\Illuminate\Support\Facades\Session::has('errors'))--}}
+                    {{--                        <div class="alert alert-success">--}}
+                    {{--                            <div>{{session('errors')}}</div>--}}
+                    {{--                        </div>--}}
+                    {{--                    @endif--}}
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <form method="post" action="/administrator/brands">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="name">نام</label>
+                                    <input type="text" name="title" class="form-control"
+                                           placeholder="نام برند را وارد کنید...">
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">توضیحات</label>
+                                    <textarea type="text" name="description" class="form-control"
+                                              placeholder="توضیحات برند را وارد کنید..."></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="photo">تصویر</label>
+                                    <input type="hidden" name="photo_id" id="brand-photo">
+                                    <div id="photo" class="dropzone"></div>
+                                </div>
 
-                            <button type="submit" class="btn btn-success pull-left">ذخیره</button>
-                        </form>
+                                <button type="submit" class="btn btn-success pull-left">ذخیره</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
             </div>
         </div>
         <!-- /.box-footer -->
@@ -53,7 +59,15 @@
     <script type="text/javascript" src="{{asset('/admin/dist/js/dropezone.js')}}"></script>
     <script>
         let drop = new Dropzone('#photo', {
-            url: "{{route('photos.create')}}"
+            addRemoveLinks: true,
+            maxFiles: 1,
+            url: "{{route('photos.upload')}}",
+            sending: function (file, xhr, formData) {
+                formData.append('_token', "{{csrf_token()}}")
+            },
+            success: function (file, response) {
+                document.getElementById('brand-photo').value = response.photo_id
+            }
         });
     </script>
 @endsection
