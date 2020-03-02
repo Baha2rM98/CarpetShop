ba028070429e68ef42ba69bea2c11c8ce25fdb73
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-02 17:44:21 +0330

    Brands route added

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::prefix('/administrator')->group(function () {
     Route::resource('/categories', 'Backend\CategoryController');
     Route::resource('/attributes-group', 'Backend\AttributeGroupController');
     Route::resource('/attributes-value', 'Backend\AttributeValueController');
+    Route::resource('/brands', 'Backend\BrandController');
 });
