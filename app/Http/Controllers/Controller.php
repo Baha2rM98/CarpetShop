bc02f96dd1f57cc3c533b3dacac058131b38956f
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-02 15:12:24 +0330

    A method added to test database connection

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Checks if database connection is set or not
+     * @return boolean|DB
+     */
+    protected function isDatabaseConnected()
+    {
+        try {
+            return DB::connection()->getPdo();
+        } catch (Exception $exception) {
+        }
+        return false;
+    }
 }
