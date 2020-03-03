5b04d70cdadde570d65c5b57a502e6c76db4779d
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-04 02:57:45 +0330

    Edit page done...

--- resources/views/admin/brands/edit.blade.php
+++ resources/views/admin/brands/edit.blade.php
@@ -1,10 +1,13 @@
 @extends('admin.layout.master')
-<title>صفحه مدیریت فروشگاه - ویرایش دسته بندی</title>
+<title>صفحه مدیریت فروشگاه - ویرایش برند</title>
+@section('styles')
+    <link rel="stylesheet" href="{{asset('/admin/dist/css/dropezone.css')}}">
+@endsection
 @section('content')
     <section class="content">
         <div class="box box-info">
             <div class="box-header with-border">
-                <h3 class="box-title pull-right">ویرایش دسته بندی {{$category->name}}</h3>
+                <h3 class="box-title pull-right">ویرایش برند {{$brand->name}}</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
@@ -19,43 +22,29 @@
                 @endif
                 <div class="row">
                     <div class="col-md-6 col-md-offset-3">
-                        <form method="post" action="/administrator/categories/{{$category->id}}">
+                        <form method="post" action="/administrator/brands/{{$brand->id}}">
                             @csrf
                             <input type="hidden" name="_method" value="PATCH">
                             <div class="form-group">
+                                <label for="name">تصویر برند</label>
+                                {{--                                --}}
+                                <img src="{{$brand->photo->path}}"  style="height:50vh">
+                            </div>
+                            <div class="form-group">
                                 <label for="name">نام</label>
-                                <input type="text" name="name" class="form-control" value="{{$category->name}}"
-                                       placeholder="عنوان دسته بندی را وارد کنید...">
+                                <input type="text" name="title" class="form-control" value="{{$brand->title}}"
+                                       placeholder="نام برند را وارد کنید...">
                             </div>
                             <div class="form-group">
-                                <label for="name">دسته والد</label>
-                                <select name="category_parent" id="" class="form-control">
-                                    <option value="">بدون والد</option>
-                                    @foreach($categories as $showParentCategory)
-                                        <option value="{{$showParentCategory->id}}"
-                                                @if($category->parent_id === $showParentCategory->id) selected @endif>{{$showParentCategory->name}}</option>
-                                        @if(count($showParentCategory->children) > 0)
-                                            @include('admin.partials.subcategory', ['categories' => $showParentCategory->children, 'level' => 1, 'selectedCategory' => $category])
-                                        @endif
-                                    @endforeach
-                                </select>
+                                <label for="name">توضیحات</label>
+                                <textarea type="text" name="description" class="form-control"
+                                          placeholder="توضیحات برند را وارد کنید...">{{$brand->description}}</textarea>
                             </div>
                             <div class="form-group">
-                                <label for="meta_title">عنوان سئو</label>
-                                <input type="text" name="meta_title" class="form-control"
-                                       value="{{$category->meta_title}}"
-                                       placeholder="عنوان سئو را وارد کنید...">
-                            </div>
-                            <div class="form-group">
-                                <label for="meta_desc">توضیحات سئو</label>
-                                <textarea name="meta_desc" class="form-control"
-                                          placeholder="توضیحات سئو را وارد کنید...">{{$category->meta_desc}}</textarea>
-                            </div>
-                            <div class="form-group">
-                                <label for="meta_keywords">کلمات کلیدی سئو</label>
-                                <input type="text" name="meta_keywords" class="form-control"
-                                       value="{{$category->meta_keywords}}"
-                                       placeholder="کلمات کلیدی سئو را وارد کنید...">
+                                <label for="photo">تصویر (فایل آپلود شده حتما باید از نوع عکس باشد و حجم آن حداکثر 4
+                                    مگابایت باشد.)</label>
+                                <input type="hidden" name="photo_id" id="brand-photo" value="{{$brand->photo_id}}">
+                                <div id="photo" class="dropzone"></div>
                             </div>
                             <button type="submit" class="btn btn-facebook pull-left">ویرایش کردن</button>
                         </form>
@@ -65,4 +54,21 @@
         </div>
         <!-- /.box-footer -->
     </section>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript" src="{{asset('/admin/dist/js/dropezone.js')}}"></script>
+    <script>
+        let drop = new Dropzone('#photo', {
+            addRemoveLinks: true,
+            maxFiles: 1,
+            url: "{{route('photos.upload')}}",
+            sending: function (file, xhr, formData) {
+                formData.append('_token', "{{csrf_token()}}")
+            },
+            success: function (file, response) {
+                document.getElementById('brand-photo').value = response.photo_id
+            }
+        });
+    </script>
 @endsection
