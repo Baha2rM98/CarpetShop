e2f7a4c2ac6d4f1a72096966d2c281a15f78fd1e
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-03 21:19:21 +0330

    Wait until we develop authentication system...

--- app/Http/Controllers/Backend/PhotoController.php
+++ app/Http/Controllers/Backend/PhotoController.php
@@ -3,83 +3,42 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Photo;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Throwable;
 
 class PhotoController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Stores a newly uploaded file in storage.
      *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse|Response|RedirectResponse
+     * @throws Throwable
      */
-    public function index()
+    public function uploadPhoto(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if (!$this->isDatabaseConnected()) {
+            abort(500, 'Database Connection Error');
+        }
+        $this->validate($request, ['file' => ['bail', 'image', 'max:4096']], ['file.image' => 'فایل آپلود شده باید از نوع عکس باشد!',
+            'file.max' => 'حجم تصویر آپلود شده نمیتواند بیشتر از 8 مگابایت باشد!']);
+        $uploadedFile = $request->file('file');
+        $fileName = time() . $uploadedFile->getClientOriginalName();
+        $originalName = $uploadedFile->getClientOriginalName();
+        Storage::disk('local')->putFileAs('photos', $uploadedFile, $fileName);
+        $photo = new Photo();
+        $photo->original_name = $originalName;
+        $photo->path = $fileName;
+        //TODO: We complete $photo->user_id = (?) after we initialized authentication system.
+        $photo->user_id = 1;
+        //TODO ------------------------------------------------------------------------------
+        $photo->saveOrFail();
+        return response()->json(['photo_id' => $photo->id]);
     }
 }
