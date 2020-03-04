9331308ee40fe78d5fa16e6707d29b1623339bbf
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-04 18:21:06 +0330

    A new method added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class Controller extends BaseController
 {
@@ -24,5 +25,16 @@ class Controller extends BaseController
         } catch (Exception $exception) {
         }
         return false;
+    }
+
+    /** Returns absolute path of uploaded files on server
+     *
+     * @param string $path
+     * @param string $dir
+     * @return string|string[]
+     */
+    protected function getFileAbsolutePath($dir, $path)
+    {
+        return Str::replaceArray('/storage/' . $dir . '/', [''], $path);
     }
 }
