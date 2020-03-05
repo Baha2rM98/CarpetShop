180fa660666099dd47b41780a9d0fed78e24f466
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-06 00:34:13 +0330

    Bugs fixed!

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -331,16 +330,6 @@
                     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
             </div>
-            <!-- search form -->
-        {{--            <form action="#" method="get" class="sidebar-form">--}}
-        {{--                <div class="input-group">--}}
-        {{--                    <input type="text" name="q" class="form-control" placeholder="جستجو...">--}}
-        {{--                    <span class="input-group-btn">--}}
-        {{--                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>--}}
-        {{--                </button>--}}
-        {{--              </span>--}}
-        {{--                </div>--}}
-        {{--            </form>--}}
         <!-- /.search form -->
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <ul class="sidebar-menu">
@@ -382,7 +371,7 @@
                 </li>
                 <li class="treeview">
                     <a href="#">
-                        <i class="fa fa-cart-plus"></i>
+                        <i class="fa fa-database"></i>
                         <span>محصولات</span>
                         <i class="fa fa-angle-left pull-right"></i>
                     </a>
