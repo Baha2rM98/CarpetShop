62697b64403c2c753d13b46c2028ce58e940cfba
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-06 04:23:44 +0330

    route method name change

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/categories', 'Backend\CategoryController@apiIndex');
+Route::get('/categories', 'Backend\CategoryController@apiVueJsCategories');
