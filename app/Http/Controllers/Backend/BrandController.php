80a3068a5c006b7732c8dcbf88ab2b0737fb0968
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-17 21:32:52 +0330

    Extra imports omitted

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use Throwable;
