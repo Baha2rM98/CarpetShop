764f35c028e496554afc79235865576c88ac0361
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-19 05:22:31 +0330

    Testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/register');
 
         $response->assertStatus(200);
     }
