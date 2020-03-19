1e093e6f911af14cad67265477291afda92a6644
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-03-19 04:46:20 +0330

    Almost done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,45 +1,74 @@
 @extends('frontend.layout.master')
 
 @section('content')
-
-
     <div class="row">
         <!--Middle Part Start-->
         <div class="col-sm-9" id="content">
             <h1 class="title">ثبت نام حساب کاربری</h1>
-            <p>اگر قبلا حساب کاربریتان را ایجاد کرد اید جهت ورود به <a href="{{route('login')}}">صفحه لاگین</a> مراجعه کنید.</p>
-            <form class="form-horizontal" method="POST" action="{{route('register')}}">
+            <p>اگر قبلا حساب کاربریتان را ایجاد کرد اید جهت ورود به <a href="{{route('login')}}">صفحه لاگین</a> مراجعه
+                کنید.</p>
+            <form class="form-horizontal" method="post" action="{{route('user.register')}}">
                 @csrf
                 <fieldset id="account">
                     <legend>اطلاعات شخصی شما</legend>
                     <div class="form-group required">
                         <label for="input-firstname" class="col-sm-2 control-label">نام</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="input-firstname" placeholder="نام" value="" name="name">
+                            <input type="text" class="form-control" id="input-firstname" placeholder="نام" value=""
+                                   name="name">
+                            @if($errors->has('name'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('name')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group required">
                         <label for="input-lastname" class="col-sm-2 control-label">نام خانوادگی</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="input-lastname" placeholder="نام خانوادگی" value="" name="last_name">
+                            <input type="text" class="form-control" id="input-lastname" placeholder="نام خانوادگی"
+                                   value="" name="last_name">
+                            @if($errors->has('last_name'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('last_name')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group required">
                         <label for="input-email" class="col-sm-2 control-label">آدرس ایمیل</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="input-email" placeholder="آدرس ایمیل" value="" name="email">
+                            <input type="email" class="form-control" id="input-email" placeholder="آدرس ایمیل" value=""
+                                   name="email">
+                            @if($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('email')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group required">
                         <label for="input-telephone" class="col-sm-2 control-label">شماره تلفن</label>
                         <div class="col-sm-10">
-                            <input type="tel" class="form-control" id="input-telephone" placeholder="شماره تلفن" value="" name="phone_number">
+                            <input type="tel" class="form-control" id="input-telephone" placeholder="شماره تلفن"
+                                   value="" name="phone_number">
+                            @if($errors->has('phone_number'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('phone_number')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="input-fax" class="col-sm-2 control-label">کدملی</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="input-fax" placeholder="کدملی" value="" name="national_code">
+                            <input type="text" class="form-control" id="input-fax" placeholder="کدملی" value=""
+                                   name="national_code">
+                            @if($errors->has('national_code'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('national_code')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                 </fieldset>
@@ -48,144 +77,91 @@
                     <div class="form-group">
                         <label for="input-company" class="col-sm-2 control-label">شرکت</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="input-company" placeholder="شرکت" value="" name="company">
+                            <input type="text" class="form-control" id="input-company" placeholder="شرکت" value=""
+                                   name="company">
+                            @if($errors->has('company'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('company')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group required">
                         <label for="input-address-1" class="col-sm-2 control-label">آدرس</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="input-address" placeholder="آدرس" value="" name="address">
+                            <input type="text" class="form-control" id="input-address" placeholder="آدرس" value=""
+                                   name="address">
+                            @if($errors->has('address'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('address')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group required">
                         <label for="input-postcode" class="col-sm-2 control-label">کد پستی</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="input-postcode" placeholder="کد پستی" value="" name="post_code">
+                            <input type="text" class="form-control" id="input-postcode" placeholder="کد پستی" value=""
+                                   name="post_code">
+                            @if($errors->has('post_code'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('post_code')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
-                <select-city-province-component></select-city-province-component>
+                    <select-city-province-component></select-city-province-component>
+                    <div class="col-sm-10" style="margin-right: 129px">
+                        @if($errors->has('province'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('province')}}</strong>
+                            </span>
+                        @endif
+                        @if($errors->has('city'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('city')}}</strong>
+                            </span>
+                        @endif
+                    </div>
                 </fieldset>
+{{--                <div class="form-group required" style="right: 1000px">--}}
+{{--                </div>--}}
                 <fieldset>
                     <legend>رمز عبور شما</legend>
                     <div class="form-group required">
                         <label for="input-password" class="col-sm-2 control-label">رمز عبور</label>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="input-password" placeholder="رمز عبور" value="" name="password">
+                            <input type="password" class="form-control" id="input-password" placeholder="رمز عبور"
+                                   value="" name="password">
+                            @if($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('password')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                     <div class="form-group required">
                         <label for="input-confirm" class="col-sm-2 control-label">تکرار رمز عبور</label>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="input-confirm" placeholder="تکرار رمز عبور" value="" name="confirm">
+                            <input type="password" class="form-control" id="input-confirm" placeholder="تکرار رمز عبور"
+                                   value="" name="password_confirmation">
+                            @if($errors->has('password_confirmation'))
+                                <span class="invalid-feedback" role="alert">
+                                <strong>{{$errors->first('password_confirmation')}}</strong>
+                            </span>
+                            @endif
                         </div>
                     </div>
                 </fieldset>
                 <div class="buttons">
                     <div class="pull-right">
-                        <input type="submit" class="btn btn-primary" value="ثبت">
+                        <input type="submit" class="btn btn-primary" value="ثبت نام">
                     </div>
                 </div>
             </form>
         </div>
         <!--Middle Part End -->
-        <!--Right Part Start -->
-{{--        <aside id="column-right" class="col-sm-3 hidden-xs">--}}
-{{--            <h3 class="subtitle">حساب کاربری</h3>--}}
-{{--            <div class="list-group">--}}
-{{--                <ul class="list-item">--}}
-{{--                    <li><a href="{{route('login')}}">ورود</a></li>--}}
-{{--                    <li><a href="{{route('register')}}">ثبت نام</a></li>--}}
-{{--                    <li><a href="#">فراموشی رمز عبور</a></li>--}}
-{{--                    <li><a href="#">حساب کاربری</a></li>--}}
-{{--                    <li><a href="#">لیست آدرس ها</a></li>--}}
-{{--                    <li><a href="wishlist.html">لیست علاقه مندی</a></li>--}}
-{{--                    <li><a href="#">تاریخچه سفارشات</a></li>--}}
-{{--                    <li><a href="#">دانلود ها</a></li>--}}
-{{--                    <li><a href="#">امتیازات خرید</a></li>--}}
-{{--                    <li><a href="#">بازگشت</a></li>--}}
-{{--                    <li><a href="#">تراکنش ها</a></li>--}}
-{{--                    <li><a href="#">خبرنامه</a></li>--}}
-{{--                    <li><a href="#">پرداخت های تکرار شونده</a></li>--}}
-{{--                </ul>--}}
-{{--            </div>--}}
-{{--        </aside>--}}
-        <!--Right Part End -->
     </div>
-
-
-{{--<div class="container">--}}
-{{--    <div class="row justify-content-center">--}}
-{{--        <div class="col-md-8">--}}
-{{--            <div class="card">--}}
-{{--                <div class="card-header">{{ __('Register') }}</div>--}}
-
-{{--                <div class="card-body">--}}
-{{--                    <form method="POST" action="{{ route('register') }}">--}}
-{{--                        @csrf--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>--}}
-
-{{--                                @error('name')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">--}}
-
-{{--                                @error('email')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">--}}
-
-{{--                                @error('password')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row mb-0">--}}
-{{--                            <div class="col-md-6 offset-md-4">--}}
-{{--                                <button type="submit" class="btn btn-primary">--}}
-{{--                                    {{ __('Register') }}--}}
-{{--                                </button>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
 @endsection
 
 @section('script-vuejs')
