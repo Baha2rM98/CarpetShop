ffce0f0116b93914b6f78c8cbaccd2fcbf6eb4e1
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-22 03:36:56 +0430

    Just dealing with unknown errors :|

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -27,7 +27,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        if ( ! $this->isDatabaseConnected()) {
+        if (!$this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
         $products = Product::paginate(10);
@@ -44,7 +44,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        if ( ! $this->isDatabaseConnected()) {
+        if (!$this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
         $brands = Brand::all();
@@ -64,18 +64,18 @@ class ProductController extends Controller
      */
     public function store(CreateProductRequest $request)
     {
-        if ( ! $this->isDatabaseConnected()) {
+        if (!$this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
-        $product                 = new Product();
-        $product->title          = $request->input('title');
-        $product->sku            = $this->generateSKU();
-        $product->slug           = static::makeSlug($request->input('slug'));
-        $product->status         = $request->input('status');
-        $product->price          = $request->input('price');
+        $product = new Product();
+        $product->title = $request->input('title');
+        $product->sku = $this->generateSKU();
+        $product->slug = static::makeSlug($request->input('slug'));
+        $product->status = $request->input('status');
+        $product->price = $request->input('price');
         $product->discount_price = $request->input('discount_price');
-        $product->description    = $request->input('description');
-        $product->brand_id       = $request->input('brand');
+        $product->description = $request->input('description');
+        $product->brand_id = $request->input('brand');
         //TODO: We complete $product->user_id = (?) after we initialized authentication system.
         $product->user_id = 1;
         //TODO --------------------------------------------------------------------------------
@@ -97,11 +97,11 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        if ( ! $this->isDatabaseConnected()) {
+        if (!$this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
 
-        $brands  = Brand::all();
+        $brands = Brand::all();
         $product = Product::with('brand', 'categories', 'attributeValues', 'photos')->whereId($id)->first();
 
         return view('admin.products.edit', compact('product', 'brands'));
@@ -118,45 +118,45 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if ( ! $this->isDatabaseConnected()) {
+        if (!$this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
         $this->validate($request, [
-            'title'          => ['bail', 'required', 'min:2', 'max:100'],
-            'slug'           => ['bail', 'required', new CheckUniqueSlugForProduct($id), 'max:100'],
-            'categories'     => 'required|array',
-            'brand'          => 'required',
-            'status'         => 'required',
-            'price'          => ['bail', 'required', 'numeric', 'digits_between:1,25'],
+            'title' => ['bail', 'required', 'min:2', 'max:100'],
+            'slug' => ['bail', 'required', new CheckUniqueSlugForProduct($id), 'max:100'],
+            'categories' => 'required|array',
+            'brand' => 'required',
+            'status' => 'required',
+            'price' => ['bail', 'required', 'numeric', 'digits_between:1,25'],
             'discount_price' => ['bail', 'nullable', 'numeric', 'digits_between:1,25'],
-            'description'    => ['bail', 'required', 'max:100000'],
-            'photo_id.*'     => 'required'
+            'description' => ['bail', 'required', 'max:100000'],
+            'photo_id.*' => 'required'
         ], [
-            'title.required'                => 'نام محصول نمیتواند خالی باشد!',
-            'title.min'                     => 'نام محصول نمیتواند کمتر از 2 کاراکتر باشد!',
-            'title.max'                     => 'نام محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
-            'slug.required'                 => 'نام مستعار محصول نمیتواند خالی باشد!',
-            'slug.max'                      => 'نام مستعار محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
-            'categories.required'           => 'هر محصول باید حداقل متعلق به یک دسته بندی باشد!',
-            'brand.required'                => 'هر محصول باید متعلق به یک برند باشد!',
-            'status.required'               => 'وضعیت محصول باید درج شود!',
-            'price.required'                => 'قیمت محصول نمیتواند خالی باشد!',
-            'price.numeric'                 => 'قیمت محصول باید از نوع عدد باشد!',
-            'price.digits_between'          => 'قیمت وارد شده بزرگ تر از حد مجاز است!',
-            'discount_price.numeric'        => 'قیمت ویژه محصول باید از نوع عدد باشد!',
+            'title.required' => 'نام محصول نمیتواند خالی باشد!',
+            'title.min' => 'نام محصول نمیتواند کمتر از 2 کاراکتر باشد!',
+            'title.max' => 'نام محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
+            'slug.required' => 'نام مستعار محصول نمیتواند خالی باشد!',
+            'slug.max' => 'نام مستعار محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
+            'categories.required' => 'هر محصول باید حداقل متعلق به یک دسته بندی باشد!',
+            'brand.required' => 'هر محصول باید متعلق به یک برند باشد!',
+            'status.required' => 'وضعیت محصول باید درج شود!',
+            'price.required' => 'قیمت محصول نمیتواند خالی باشد!',
+            'price.numeric' => 'قیمت محصول باید از نوع عدد باشد!',
+            'price.digits_between' => 'قیمت وارد شده بزرگ تر از حد مجاز است!',
+            'discount_price.numeric' => 'قیمت ویژه محصول باید از نوع عدد باشد!',
             'discount_price.digits_between' => 'قیمت ویژه وارد شده بزرگ تر از حد مجاز است!',
-            'description.required'          => 'توضیحات محصول نمیتواند خالی باشد!',
-            'description.max'               => 'توضیحات محصول نمیتواند بیشتر ار 100000 کاراکتر باشد!',
-            'photo_id.*.required'           => 'عکس محصول نمیتواند خالی باشد!'
+            'description.required' => 'توضیحات محصول نمیتواند خالی باشد!',
+            'description.max' => 'توضیحات محصول نمیتواند بیشتر ار 100000 کاراکتر باشد!',
+            'photo_id.*.required' => 'عکس محصول نمیتواند خالی باشد!'
         ]);
-        $product                 = Product::findOrFail($id);
-        $product->title          = $request->title;
-        $product->slug           = static::makeSlug($request->slug);
-        $product->status         = $request->status;
-        $product->price          = $request->price;
+        $product = Product::findOrFail($id);
+        $product->title = $request->title;
+        $product->slug = static::makeSlug($request->slug);
+        $product->status = $request->status;
+        $product->price = $request->price;
         $product->discount_price = $request->discount_price;
-        $product->description    = $request->description;
-        $product->brand_id       = $request->brand;
+        $product->description = $request->description;
+        $product->brand_id = $request->brand;
         //TODO: We complete $product->user_id = (?) after we initialized authentication system.
         $product->user_id = 1;
         //TODO --------------------------------------------------------------------------------
@@ -178,7 +178,7 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        if ( ! $this->isDatabaseConnected()) {
+        if (!$this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
         $product = Product::with('photos')->whereId($id)->first();
@@ -256,7 +256,7 @@ class ProductController extends Controller
      */
     public function apiVueJsGetCategoriesAttributes(Request $request)
     {
-        $categories     = $request->all();
+        $categories = $request->all();
         $attributeGroup = AttributeGroup::with('attributeValues', 'categories')->whereHas(
             'categories', function ($query) use ($categories) {
             $query->whereIn('categories.id', $categories);
