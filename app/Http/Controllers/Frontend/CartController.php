93f6633f05a9e00c3530fb5ca8f3e34d8109be39
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-22 03:55:00 +0430

    Cart controller almost done...

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Cart;
 use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
-    //
+    /**
+     * Adds a product to session
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     *
+     * @return RedirectResponse
+     */
+    public function addToCart(Request $request, $id)
+    {
+        $product = Product::with('photos')->whereId($id)->first();
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+        $request->session()->put('cart', $cart);
+        return back();
+    }
+
+    /**
+     * Removes a product from a session
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     *
+     * @return RedirectResponse
+     */
+    public function removeItem(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->remove($product, $product->id);
+        $request->session()->put('cart', $cart);
+        return back();
+    }
 }
