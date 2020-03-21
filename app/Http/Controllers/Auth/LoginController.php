5faffaa3a84b1dee900e9655cf517b245d7421e7
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-22 03:30:15 +0430

    Due to tests throttle middleware blocked development ip (127.0.0.1), in the end they will be activated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,7 +36,8 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware(['guest', 'throttle:100,1'])->except('logout');
+//        $this->middleware(['guest', 'throttle:100,1'])->except('logout');
+        $this->middleware('guest')->except('logout');
     }
 
     /**
