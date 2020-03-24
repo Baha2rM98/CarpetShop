ad1d447999545e63b90a49ad0f391a9c1d9d3a18
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-25 02:04:15 +0430

    New migrations added

---
+++ database/migrations/2020_03_24_212222_create_coupon_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupon_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('coupon_id')->unsigned();
+            $table->foreign('coupon_id')->references('id')->on('coupons')->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupon_user');
+    }
+}
