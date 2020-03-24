61f75c94bd493e74d3f7d03296bfb436c68a5dd8
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-24 05:52:00 +0430

    Some methods edited

--- app/Http/Controllers/Frontend/ShopHomeController.php
+++ app/Http/Controllers/Frontend/ShopHomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
 
@@ -21,32 +20,32 @@ class ShopHomeController extends Controller
         if ( ! $this->isDatabaseConnected()) {
             abort(500, 'Database Connection Error');
         }
-        $products = Product::orderBy('created_at', 'desc')->limit(5)->get();
+        $products = Product::orderBy('created_at', 'desc')->get();
 
         return view('frontend.home.index', compact('products'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
+//    /**
+//     * Show the form for creating a new resource.
+//     *
+//     * @return Response
+//     */
+//    public function create()
+//    {
+//        //
+//    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     *
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+//    /**
+//     * Store a newly created resource in storage.
+//     *
+//     * @param  Request  $request
+//     *
+//     * @return Response
+//     */
+//    public function store(Request $request)
+//    {
+//        //
+//    }
 
     /**
      * Display the specified resource.
@@ -60,40 +59,40 @@ class ShopHomeController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+//    /**
+//     * Show the form for editing the specified resource.
+//     *
+//     * @param  int  $id
+//     *
+//     * @return Response
+//     */
+//    public function edit($id)
+//    {
+//        //
+//    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     *
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+//    /**
+//     * Update the specified resource in storage.
+//     *
+//     * @param  Request  $request
+//     * @param  int  $id
+//     *
+//     * @return Response
+//     */
+//    public function update(Request $request, $id)
+//    {
+//        //
+//    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+//    /**
+//     * Remove the specified resource from storage.
+//     *
+//     * @param  int  $id
+//     *
+//     * @return Response
+//     */
+//    public function destroy($id)
+//    {
+//        //
+//    }
 }
