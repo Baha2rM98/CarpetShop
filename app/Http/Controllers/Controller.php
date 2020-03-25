36c533bee7c1097259cc54c579913c2dbd29960a
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-25 20:17:17 +0430

    getAbsolutePath method removed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,43 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
-
-    /**
-     * Checks if database connection is set or not
-     * @return bool|DB
-     */
-    protected function isDatabaseConnected()
-    {
-        try {
-            return DB::connection()->getPdo();
-        } catch (Exception $exception) {
-        }
-
-        return false;
-    }
-
-    /** Returns absolute path of uploaded files on server
-     *
-     * @param  string  $path
-     * @param  string  $dir
-     *
-     * @return string
-     */
-    public static function getFileAbsolutePath($dir, $path)
-    {
-        return Str::replaceArray('/storage/'.$dir.'/', [''], $path);
-    }
 }
