038f82133f7741a32038cc5b6db7cf831625b734
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-29 00:11:33 +0430

    Almost done...

--- app/Coupon.php
+++ app/Coupon.php
@@ -5,8 +5,23 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
+/**
+ * @property mixed title
+ * @property mixed code
+ * @property mixed price
+ * @property mixed status
+ */
 class Coupon extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'code', 'price', 'status'
+    ];
+
     /**
      * Returns a many-to-many relationship with User
      * @return Relation
