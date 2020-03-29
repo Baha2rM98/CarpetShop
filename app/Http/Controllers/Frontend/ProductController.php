2e889bbbe7d0538e349d8e12daaa7dd2e76365c8
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-30 03:54:30 +0430

    ProductController on shop side created and a method added

---
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    /**
+     * Returns a product, its features and its related products
+     *
+     * @param  string  $slug
+     * @return Factory|View
+     */
+    public function getProduct($slug)
+    {
+        $product = Product::with('categories', 'brand', 'attributeValues.attributeGroup', 'photos')
+            ->whereSlug($slug)->first();
+        $relatedProducts = Product::with('categories')
+            ->whereHas('categories', function ($query) use ($product) {
+                $query->whereIn('categories.id', $product->categories);
+            })->get();
+        return view('frontend.products.index', compact('product', 'relatedProducts'));
+    }
+}
