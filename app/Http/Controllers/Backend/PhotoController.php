e065465218f317f0920b8a36203738071d19ea15
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-30 23:32:24 +0430

    Bugs fixed

--- app/Http/Controllers/Backend/PhotoController.php
+++ app/Http/Controllers/Backend/PhotoController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class PhotoController extends Controller
@@ -17,44 +17,36 @@ class PhotoController extends Controller
     /**
      * Stores a newly uploaded file in storage.
      *
-     * @param Request $request
+     * @param  Request  $request
      * @return JsonResponse|Response|RedirectResponse
      * @throws Throwable
      */
     public function uploadPhoto(Request $request)
     {
-        $this->validate($request, ['file' => ['bail', 'image', 'max:4096']], ['file.image' => 'فایل آپلود شده باید از نوع عکس باشد!',
-            'file.max' => 'حجم تصویر آپلود شده نمیتواند بیشتر از 8 مگابایت باشد!']);
+        $this->photoValidator($request);
         $uploadedFile = $request->file('file');
-        $fileName = time() . $uploadedFile->getClientOriginalName();
+        $fileName = time().$uploadedFile->getClientOriginalName();
         $originalName = $uploadedFile->getClientOriginalName();
-        Storage::disk('local')->putFileAs('public/photos', $uploadedFile, $fileName);
-//        $this->removeDuplicateFiles();
         $photo = new Photo();
         $photo->original_name = $originalName;
         $photo->path = $fileName;
-        //TODO: We complete $photo->user_id = (?) after we initialized authentication system.
-        $photo->user_id = 1;
-        //TODO ------------------------------------------------------------------------------
         $photo->saveOrFail();
+        Storage::disk('local')->putFileAs('public/photos', $uploadedFile, $fileName);
         return response()->json(['photo_id' => $photo->id]);
     }
 
-//TODO
-//    /**
-//     * @deprecated
-//     */
-//    private function removeDuplicateFiles()
-//    {
-//        $files = Storage::disk('local')->allFiles('public/photos');
-//        $photos = Photo::all();
-//        foreach ($files as $file) {
-//            foreach ($photos as $photo) {
-//                if ($file === 'public/photos/' . $this->getFileAbsolutePath('photos', $photo->path)) {
-//                    Storage::disk('local')->delete($file);
-//                    $photo->delete();
-//                }
-//            }
-//        }
-//    }
+    /**
+     * Validates uploaded photos
+     *
+     * @param  Request  $request
+     * @return array
+     * @throws ValidationException
+     */
+    private function photoValidator(Request $request)
+    {
+        return $this->validate($request, ['file' => ['bail', 'image', 'max:4096']], [
+            'file.image' => 'فایل آپلود شده باید از نوع عکس باشد!',
+            'file.max' => 'حجم تصویر آپلود شده نمیتواند بیشتر از 8 مگابایت باشد!'
+        ]);
+    }
 }
