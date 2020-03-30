7755db0c214c705e1ba8ab18ffe7d26d74312f79
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-03-31 03:44:31 +0430

    Synced with model

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -6,7 +6,6 @@ use App\AttributeGroup;
 use App\Brand;
 use App\Category;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\CreateProductRequest;
 use App\Product;
 use App\Rules\CheckUniqueSlugForProduct;
 use Illuminate\Contracts\View\Factory;
@@ -15,6 +14,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 use Throwable;
 
@@ -51,25 +51,17 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  CreateProductRequest  $request
+     * @param  Request  $request
      *
      * @return RedirectResponse|Redirector
      * @throws Throwable
      */
-    public function store(CreateProductRequest $request)
+    public function store(Request $request)
     {
-        $product = new Product();
-        $product->title = $request->input('title');
-        $product->sku = $this->generateSKU();
-        $product->slug = static::makeSlug($request->input('slug'));
-        $product->status = $request->input('status');
-        $product->price = $request->input('price');
-        $product->discount_price = $request->input('discount_price');
-        $product->description = $request->input('description');
-        $product->brand_id = $request->input('brand');
-        //TODO: We complete $product->user_id = (?) after we initialized authentication system.
-        $product->user_id = 1;
-        //TODO --------------------------------------------------------------------------------
+        $this->productValidator($request);
+        $product = new Product($request->all());
+        $product->sku = $this->generateSKU(Product::all()->count() + 1);
+        $product->slug = self::makeSlug($request->input('slug'));
         $product->saveOrFail();
         $product->categories()->sync($request->input('categories'));
         $product->attributeValues()->sync($request->input('attributes'));
@@ -105,50 +97,14 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'title' => ['bail', 'required', 'min:2', 'max:100'],
-            'slug' => ['bail', 'required', new CheckUniqueSlugForProduct($id), 'max:100'],
-            'categories' => 'required|array',
-            'brand' => 'required',
-            'status' => 'required',
-            'price' => ['bail', 'required', 'numeric', 'digits_between:1,25', 'gt:discount_price'],
-            'discount_price' => ['bail', 'nullable', 'numeric', 'digits_between:1,25'],
-            'description' => ['bail', 'required', 'max:100000'],
-            'photo_id.*' => 'required'
-        ], [
-            'title.required' => 'نام محصول نمیتواند خالی باشد!',
-            'title.min' => 'نام محصول نمیتواند کمتر از 2 کاراکتر باشد!',
-            'title.max' => 'نام محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
-            'slug.required' => 'نام مستعار محصول نمیتواند خالی باشد!',
-            'slug.max' => 'نام مستعار محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
-            'categories.required' => 'هر محصول باید حداقل متعلق به یک دسته بندی باشد!',
-            'brand.required' => 'هر محصول باید متعلق به یک برند باشد!',
-            'status.required' => 'وضعیت محصول باید درج شود!',
-            'price.required' => 'قیمت محصول نمیتواند خالی باشد!',
-            'price.numeric' => 'قیمت محصول باید از نوع عدد باشد!',
-            'price.digits_between' => 'قیمت وارد شده بزرگ تر از حد مجاز است!',
-            'price.gt' => 'قیمت ویژه محصول نمیتواند از قیمت اصلی آن بزرگتر یا برابر باشد!',
-            'discount_price.numeric' => 'قیمت ویژه محصول باید از نوع عدد باشد!',
-            'discount_price.digits_between' => 'قیمت ویژه وارد شده بزرگ تر از حد مجاز است!',
-            'description.required' => 'توضیحات محصول نمیتواند خالی باشد!',
-            'description.max' => 'توضیحات محصول نمیتواند بیشتر ار 100000 کاراکتر باشد!',
-            'photo_id.*.required' => 'عکس محصول نمیتواند خالی باشد!'
-        ]);
+        $this->productValidator($request);
         $product = Product::findOrFail($id);
-        $product->title = $request->title;
-        $product->slug = static::makeSlug($request->slug);
-        $product->status = $request->status;
-        $product->price = $request->price;
-        $product->discount_price = $request->discount_price;
-        $product->description = $request->description;
-        $product->brand_id = $request->brand;
-        //TODO: We complete $product->user_id = (?) after we initialized authentication system.
-        $product->user_id = 1;
-        //TODO --------------------------------------------------------------------------------
+        $product->fill($request->all());
+        $product->slug = self::makeSlug($request->input('slug'));
         $product->saveOrFail();
-        $product->categories()->sync($request->categories);
+        $product->categories()->sync($request->input('categories'));
         $product->attributeValues()->sync($request->input('attributes'));
-        $product->photos()->sync(explode(',', $request->photo_id[0]));
+        $product->photos()->sync(explode(',', $request->input('photo_id')[0]));
         Session::flash('products', 'محصول با موفقیت ویرایش شد!');
 
         return redirect('/administrator/products');
@@ -171,18 +127,59 @@ class ProductController extends Controller
     }
 
     /**
+     * Validates product features
+     *
+     * @param  Request  $request
+     * @return array
+     * @throws ValidationException
+     */
+    private function productValidator(Request $request)
+    {
+        return $this->validate($request, [
+            'title' => ['bail', 'required', 'min:2', 'max:100'],
+            'slug' => ['bail', 'required', new CheckUniqueSlugForProduct(), 'max:100'],
+            'categories' => ['required', 'array'],
+            'brand_id' => 'required',
+            'status' => 'required',
+            'price' => ['bail', 'required', 'numeric', 'digits_between:1,15', 'gt:discount_price'],
+            'discount_price' => ['bail', 'nullable', 'numeric', 'digits_between:1,15'],
+            'description' => ['bail', 'required', 'max:100000'],
+            'photo_id.*' => 'required'
+        ], [
+            'title.required' => 'نام محصول نمیتواند خالی باشد!',
+            'title.min' => 'نام محصول نمیتواند کمتر از 2 کاراکتر باشد!',
+            'title.max' => 'نام محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
+            'slug.required' => 'نام مستعار محصول نمیتواند خالی باشد!',
+            'slug.max' => 'نام مستعار محصول نمیتواند بیشتر از 100 کاراکتر باشد!',
+            'categories.required' => 'هر محصول باید حداقل متعلق به یک دسته بندی باشد!',
+            'brand_id.required' => 'هر محصول باید متعلق به یک برند باشد!',
+            'status.required' => 'وضعیت محصول باید درج شود!',
+            'price.required' => 'قیمت محصول نمیتواند خالی باشد!',
+            'price.numeric' => 'قیمت محصول باید از نوع عدد باشد!',
+            'price.digits_between' => 'قیمت وارد شده بزرگ تر از حد مجاز است!',
+            'price.gt' => 'قیمت ویژه محصول نمیتواند از قیمت اصلی آن بزرگتر یا برابر باشد!',
+            'discount_price.numeric' => 'قیمت ویژه محصول باید از نوع عدد باشد!',
+            'discount_price.digits_between' => 'قیمت ویژه وارد شده بزرگ تر از حد مجاز است!',
+            'description.required' => 'توضیحات محصول نمیتواند خالی باشد!',
+            'description.max' => 'توضیحات محصول نمیتواند بیشتر ار 100000 کاراکتر باشد!',
+            'photo_id.*.required' => 'عکس محصول نمیتواند خالی باشد!'
+        ]);
+    }
+
+    /**
      * Generates an individual identifier for each product
      *
+     * @param  int  $id
      * @return string
      */
-    private function generateSKU()
+    private function generateSKU($id)
     {
         $alphabet = array_merge(range('a', 'z'), range('A', 'Z'));
         shuffle($alphabet);
-        $sku = substr(str_shuffle(strval(mt_rand(1, mt_getrandmax())).implode(array_slice($alphabet, mt_rand(8, 20),
-                8))), 4, 8);
+        $sku = strval($id).'-'.substr(str_shuffle(strval(mt_rand(1, mt_getrandmax())).implode(
+                    array_slice($alphabet, mt_rand(8, 20), 8))), 4, 8);
         if ($this->ifSKUExists($sku)) {
-            return $this->generateSKU();
+            return $this->generateSKU($id);
         }
 
         return $sku;
@@ -208,7 +205,10 @@ class ProductController extends Controller
      */
     public static function makeSlug($slug)
     {
-        return preg_replace('/\s+/', '-', trim(str_replace(['؟', '?'], '', strtolower($slug))));
+        if (preg_match('/[A-Z]/', $slug)) {
+            return preg_replace('/\s+/', '-', trim(str_replace(['؟', '?'], '', strtolower($slug))));
+        }
+        return preg_replace('/\s+/', '-', trim(str_replace(['؟', '?'], '', $slug)));
     }
 
     /*
@@ -236,14 +236,13 @@ class ProductController extends Controller
      *
      * @return JsonResponse
      */
-    public function apiVueJsGetCategoriesAttributes(Request $request)
+    public function apiVueJsGetCategoryAttributes(Request $request)
     {
         $categories = $request->all();
-        $attributeGroup = AttributeGroup::with('attributeValues', 'categories')->whereHas(
+        $attributeGroup = AttributeGroup::with('attributeValues')->whereHas(
             'categories', function ($query) use ($categories) {
             $query->whereIn('categories.id', $categories);
         })->get();
-
         return response()->json(['attributes' => $attributeGroup]);
     }
 }
