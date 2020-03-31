262ce86b21d009a428a64e136c880a9336d576c8
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-01 04:25:12 +0430

    Synced with migration (soft delete trait added)

--- app/City.php
+++ app/City.php
@@ -4,9 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
+    use SoftDeletes;
+
     /**
      * Returns a one-to-many relationship with Province
      * @return Relation
