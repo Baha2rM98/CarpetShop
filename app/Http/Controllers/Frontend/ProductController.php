6bd4ddf83afb2129673c147eecec66113d22caea
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-03 00:18:09 +0430

    Synced with blade views

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Category;
 use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Contracts\View\Factory;
@@ -17,14 +18,32 @@ class ProductController extends Controller
      */
     public function getProduct($slug)
     {
-        $product = Product::with('brand', 'attributeValues.attributeGroup', 'photos')
+        $product = Product::with('brand', 'attributeValues.attributeGroup', 'photos', 'categories')
             ->whereSlug($slug)->first();
         if (is_null($product)) {
             abort(404);
         }
-        $relatedProducts = Product::whereHas('categories', function ($query) use ($product) {
+        $relatedProducts = Product::with('photos')->whereHas('categories', function ($query) use ($product) {
             $query->whereIn('categories.id', $product->categories);
         })->get();
         return view('frontend.products.index', compact('product', 'relatedProducts'));
+    }
+
+    /**
+     * Returns Products by their categories
+     *
+     * @param  int  $id
+     * @param  int  $page
+     * @return Factory|View
+     */
+    public function getProductsByCategory($id, $page = 10)
+    {
+        $category = Category::findOrFail($id);
+        $products = Product::with('photos')->whereHas('categories', function ($query) use ($category) {
+            $query->where('categories.id', $category->id);
+        })->paginate($page);
+        $items = Category::all();
+
+        return view('frontend.categories.index', compact('category', 'products', 'items'));
     }
 }
--- app/Http/Controllers/Frontend/ShopHomeController.php
+++ app/Http/Controllers/Frontend/ShopHomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Category;
 use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class ShopHomeController extends Controller
@@ -17,79 +17,9 @@ class ShopHomeController extends Controller
      */
     public function index()
     {
-        $products = Product::orderBy('created_at', 'desc')->get();
+        $products = Product::orderByDesc('created_at')->get();
+        $categories = Category::orderByDesc('created_at')->get();
 
-        return view('frontend.home.index', compact('products'));
+        return view('frontend.home.index', compact('products', 'categories'));
     }
-
-//    /**
-//     * Show the form for creating a new resource.
-//     *
-//     * @return Response
-//     */
-//    public function create()
-//    {
-//        //
-//    }
-
-//    /**
-//     * Store a newly created resource in storage.
-//     *
-//     * @param  Request  $request
-//     *
-//     * @return Response
-//     */
-//    public function store(Request $request)
-//    {
-//        //
-//    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     *
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-//    /**
-//     * Show the form for editing the specified resource.
-//     *
-//     * @param  int  $id
-//     *
-//     * @return Response
-//     */
-//    public function edit($id)
-//    {
-//        //
-//    }
-
-//    /**
-//     * Update the specified resource in storage.
-//     *
-//     * @param  Request  $request
-//     * @param  int  $id
-//     *
-//     * @return Response
-//     */
-//    public function update(Request $request, $id)
-//    {
-//        //
-//    }
-
-//    /**
-//     * Remove the specified resource from storage.
-//     *
-//     * @param  int  $id
-//     *
-//     * @return Response
-//     */
-//    public function destroy($id)
-//    {
-//        //
-//    }
 }
