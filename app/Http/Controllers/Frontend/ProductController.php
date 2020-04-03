682d09198ecd25d8eb320807ed225e4306ea925b
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-04 01:20:03 +0430

    New API methods registered for responding vue

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -6,6 +6,8 @@ use App\Category;
 use App\Http\Controllers\Controller;
 use App\Product;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\URL;
 use Illuminate\View\View;
 
 class ProductController extends Controller
@@ -26,24 +28,75 @@ class ProductController extends Controller
         $relatedProducts = Product::with('photos')->whereHas('categories', function ($query) use ($product) {
             $query->whereIn('categories.id', $product->categories);
         })->get();
+
         return view('frontend.products.index', compact('product', 'relatedProducts'));
     }
 
     /**
-     * Returns Products by their categories
+     * Returns categories with related products
      *
      * @param  int  $id
-     * @param  int  $page
      * @return Factory|View
+     * @see apiVueJsGetProductsByCategory
+     * @see apiVueJsGetSortedProductsByCategory
      */
-    public function getProductsByCategory($id, $page = 10)
+    public function getProductsByCategory($id)
     {
         $category = Category::findOrFail($id);
-        $products = Product::with('photos')->whereHas('categories', function ($query) use ($category) {
-            $query->where('categories.id', $category->id);
-        })->paginate($page);
+
+        // We get products by their categories by api vus js
+
         $items = Category::all();
 
-        return view('frontend.categories.index', compact('category', 'products', 'items'));
+        return view('frontend.categories.index', compact('category', 'items'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Controllers
+    |--------------------------------------------------------------------------
+    |
+    | Here is where we registered API controller for our application
+    */
+
+    /**
+     * Returns related product by their categories
+     *
+     * @param  int  $id
+     * @return JsonResponse
+     */
+    public function apiVueJsGetProductsByCategory($id)
+    {
+        $products = Product::with('photos')->whereHas('categories', function ($query) use ($id) {
+            $query->where('categories.id', $id);
+        })->paginate(10);
+
+        $baseUrl = URL::to('/');
+
+        return response()->json(['products' => $products, 'url' => $baseUrl], 200);
+    }
+
+    /**
+     * Returns related sorted product by their categories
+     *
+     * @param  int  $id
+     * @param  string  $sort
+     * @return JsonResponse
+     */
+    public function apiVueJsGetSortedProductsByCategory($id, $sort)
+    {
+        if ($sort === 'default') {
+            $products = Product::with('photos')->whereHas('categories', function ($query) use ($id) {
+                $query->where('categories.id', $id);
+            })->paginate(10);
+
+            return response()->json(['products' => $products]);
+        }
+
+        $products = Product::with('photos')->whereHas('categories', function ($query) use ($id) {
+            $query->where('categories.id', $id);
+        })->orderBy('price', $sort)->paginate(10);
+
+        return response()->json(['products' => $products], 200);
     }
 }
