2069dcb31db4f5bcad68867b561ddb2056a27b64
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-04 16:44:10 +0430

    Admin main folder renamed to dashboard

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,33 @@
+@extends('admin.layout.master')
+
+@section('content')
+    <!-- Main content -->
+    <section class="content">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3>44</h3>
+                        <a href="" style="color: white">کاربران ثبت شده</a>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+            <br>
+            <br>
+            <div class="col-sm-offset-1">
+                <h2>این نسخه دمو است. برای اطلاعات بیشتر به لیست سمت راست نگاه کنید.</h2>
+            </div>
+        </div>
+        <!-- /.row (main row) -->
+    </section>
+    <!-- /.content -->
+@endsection
