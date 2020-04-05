cff28a165b2c728c2a8b418891e8a83d4102a881
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-05 18:58:05 +0430

    Some changes in api middleware due to our application uses both web and api so we have to config in the best way

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,10 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-//            'throttle:100,1',
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
