1e70a1d50f8c2bd0ce7ee638e2105693e14b126d
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-09 23:09:31 +0430

    Comment model and its relations created

---
+++ app/Comment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property mixed user_id
+ * @property mixed product_id
+ */
+class Comment extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['comment'];
+
+    /**
+     * Returns a one-to-many relationship with User
+     * @return Relation
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Returns a one-to-many relationship with Product
+     * @return Relation
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
