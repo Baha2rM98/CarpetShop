10b13ad3ec96a083b10a4d4388a0d922b8384eb9
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-13 04:32:34 +0430

    Migration edited

--- database/migrations/2020_04_12_202522_create_order_product_table.php
+++ database/migrations/2020_04_12_202522_create_order_product_table.php
@@ -17,8 +17,9 @@ class CreateOrderProductTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('order_id')->unsigned();
             $table->foreign('order_id')->references('id')->on('orders');
-            $table->bigInteger('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('quantity');
             $table->timestamps();
             $table->softDeletes();
         });
