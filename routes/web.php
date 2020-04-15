ed6e29cc0a29cdd54ec9faeb9b4926a0bdc0d632
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-15 14:32:07 +0430

    Namespace changed (Frontend to Shop, Backend to Admin)

--- routes/web.php
+++ routes/web.php
@@ -37,88 +37,88 @@ Route::domain(Helper::getApplicationSubDomain())->group(function () {
     Route::middleware('auth:admin')->group(function () {
 
 
-        Route::get('/', 'Backend\AdminController@dashboard')->name('admin.dashboard');
+        Route::get('/', 'Admin\AdminController@dashboard')->name('admin.dashboard');
 
 
-        Route::resource('/categories', 'Backend\CategoryController');
+        Route::resource('/categories', 'Admin\CategoryController');
 
 
-        Route::get('/category/{id}/attributes', 'Backend\CategoryController@indexAttributes')->name('category.attributes');
+        Route::get('/category/{id}/attributes', 'Admin\CategoryController@indexAttributes')->name('category.attributes');
 
 
-        Route::post('/category/{id}/attributes', 'Backend\CategoryController@saveAttributes');
+        Route::post('/category/{id}/attributes', 'Admin\CategoryController@saveAttributes');
 
 
-        Route::resource('/attribute-groups', 'Backend\AttributeGroupController');
+        Route::resource('/attribute-groups', 'Admin\AttributeGroupController');
 
 
-        Route::resource('/attribute-values', 'Backend\AttributeValueController');
+        Route::resource('/attribute-values', 'Admin\AttributeValueController');
 
 
-        Route::resource('/brands', 'Backend\BrandController');
+        Route::resource('/brands', 'Admin\BrandController');
 
 
-        Route::post('/photos/upload', 'Backend\PhotoController@uploadPhoto')->name('photos.upload');
+        Route::post('/photos/upload', 'Admin\PhotoController@uploadPhoto')->name('photos.upload');
 
 
-        Route::resource('/products', 'Backend\ProductController');
+        Route::resource('/products', 'Admin\ProductController');
 
 
-        Route::resource('/coupons', 'Backend\CouponController');
+        Route::resource('/coupons', 'Admin\CouponController');
 
 
-        Route::get('/comments', 'Backend\CommentController@viewComments')->name('comment.index');
+        Route::get('/comments', 'Admin\CommentController@viewComments')->name('comment.index');
 
 
-        Route::post('/comment/{id}', 'Backend\CommentController@confirmComment')->name('comment.confirmation');
+        Route::post('/comment/{id}', 'Admin\CommentController@confirmComment')->name('comment.confirmation');
 
 
-        Route::get('/orders', 'Backend\OrderController@getOrders')->name('order.index');
+        Route::get('/orders', 'Admin\OrderController@getOrders')->name('order.index');
     });
 });
 
 
-Route::get('/', 'Frontend\ShopHomeController@index');
+Route::get('/', 'Shop\ShopHomeController@index');
 
 
-Route::get('/product/{sku}', 'Frontend\ProductController@getProduct')->name('product.introduce');
+Route::get('/product/{sku}', 'Shop\ProductController@getProduct')->name('product.introduce');
 
 
-Route::get('/category/{slug}/products', 'Frontend\ProductController@getProductsByCategory')->name('category.index');
+Route::get('/category/{slug}/products', 'Shop\ProductController@getProductsByCategory')->name('category.index');
 
 
 Auth::routes();
 Route::post('/register', 'Auth\RegisterController@register')->name('user.register');
 
 
-Route::get('/cart/add/{id}', 'Frontend\CartController@addToCart')->name('cart.add');
+Route::get('/cart/add/{id}', 'Shop\CartController@addToCart')->name('cart.add');
 
 
-Route::post('/remove/from/cart/{id}', 'Frontend\CartController@removeItem')->name('cart.remove');
+Route::post('/remove/from/cart/{id}', 'Shop\CartController@removeItem')->name('cart.remove');
 
 
-Route::get('/cart', 'Frontend\CartController@getCart')->name('cart.cart');
+Route::get('/cart', 'Shop\CartController@getCart')->name('cart.cart');
 
 
 Route::middleware('auth')->group(function () {
 
 
-    Route::get('/profile', 'Frontend\UserController@profile')->name('user.dashboard');
+    Route::get('/profile', 'Shop\UserController@profile')->name('user.dashboard');
 
 
-    Route::post('/apply-coupon', 'Frontend\CouponController@applyCoupon')->name('coupon.apply');
+    Route::post('/apply-coupon', 'Shop\CouponController@applyCoupon')->name('coupon.apply');
 
 
-    Route::post('/post-comment/{productId}', 'Frontend\CommentController@saveComment')->name('comment.post');
+    Route::post('/post-comment/{productId}', 'Shop\CommentController@saveComment')->name('comment.post');
 
 
-    Route::post('/favorite/add/{id}', 'Frontend\UserController@addToFavorite')->name('favorite.add');
+    Route::post('/favorite/add/{id}', 'Shop\UserController@addToFavorite')->name('favorite.add');
 
 
-    Route::get('/favorites', 'Frontend\UserController@getFavorites')->name('favorite.index');
+    Route::get('/favorites', 'Shop\UserController@getFavorites')->name('favorite.index');
 
 
-    Route::post('/favorite/delete/{id}', 'Frontend\UserController@deleteFromFavorites')->name('favorite.delete');
+    Route::post('/favorite/delete/{id}', 'Shop\UserController@deleteFromFavorites')->name('favorite.delete');
     
 
     // Checkout
