67407c6309ceb1c28c7b8f040ecd8a579534b9c2
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-20 22:59:26 +0430

    Payment request error view added

---
+++ resources/views/errors/payment-request-error.blade.php
@@ -0,0 +1,13 @@
+@extends('shop.layout.master')
+<title>خطا</title>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <br>
+            <br>
+            <h1 style="margin-right: 380px"><span class="label label-danger">درخواست شما با خطا مواجه شد! مجددا تلاش کنید.</span></h1>
+            <br>
+            <br>
+        </div>
+    </div>
+@endsection
