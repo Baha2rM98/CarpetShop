eb297e768b69237615e57118329267351ebc5460
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-22 22:52:02 +0430

    CouponUser model created

---
+++ app/CouponUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CouponUser extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'coupon_user';
+}
