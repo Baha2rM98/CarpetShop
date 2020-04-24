e9052f813593f8133ade1b506949284bcda864ba
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-24 20:00:21 +0430

    Almost done...

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -4,14 +4,43 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Order;
-use Illuminate\Http\Request;
+use App\OrderProduct;
+use App\Product;
+use Illuminate\View\View;
 
 class OrderController extends Controller
 {
+    /**
+     * Retrieves all orders
+     *
+     * @return View
+     */
     public function getOrders()
     {
-        $orders = Order::paginate(10);
+        $orders = Order::withTrashed()->with('products', 'payment', 'coupon', 'user', 'address.province',
+            'address.city')
+            ->paginate(10);
 
         return view('admin.orders.index', compact('orders'));
+    }
+
+    /**
+     * Retrieves products of each order
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function getOrderDetails($id)
+    {
+        $order = Order::findOrFail($id);
+
+        $products = Product::withTrashed()->with('photos')
+            ->whereHas('orders', function ($query) use ($id) {
+                $query->where('order_id', $id);
+            })->get();
+
+        $pivots = OrderProduct::where('order_id', $id)->get();
+
+        return view('admin.orders.order-products', compact('order', 'products', 'pivots'));
     }
 }
