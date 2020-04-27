5295aece064eddeb032d72776ae8b19488b1198b
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-27 19:14:05 +0430

    SMS class done...

--- app/SMSPanel/SMS.php
+++ app/SMSPanel/SMS.php
@@ -2,9 +2,54 @@
 
 namespace App\SMSPanel;
 
+use Kavenegar\Exceptions\ApiException;
+use Kavenegar\Exceptions\HttpException;
 use Kavenegar\KavenegarApi;
 
 class SMS
 {
+    /**
+     * Sends the message
+     *
+     * @param  string  $message
+     * @param  string  $receiver
+     * @return bool
+     */
+    public static function send($message, $receiver)
+    {
+        try {
+            $apiKey = env('API_KEY');
+            $sender = env('SMS_SENDER');
+            $panel = new KavenegarApi($apiKey);
+            $panel->Send($sender, $receiver, $message);
+        } catch (ApiException $apiException) {
+            return false;
+        } catch (HttpException $httpException) {
+            return false;
+        }
 
+        return true;
+    }
+
+    /**
+     * Sends the message surly (grantees the sms will be delivered)
+     *
+     * @param  string  $template
+     * @param  string  $receiver
+     * @param  string  $token
+     * @return bool
+     */
+    public static function forceSend($template, $receiver, $token)
+    {
+        try {
+            $apiKey = env('API_KEY');
+            $panel = new KavenegarApi($apiKey);
+            $panel->VerifyLookup($receiver, $token, '', '', $template);
+            return true;
+        } catch (ApiException $apiException) {
+            return false;
+        } catch (HttpException $httpException) {
+            return false;
+        }
+    }
 }
