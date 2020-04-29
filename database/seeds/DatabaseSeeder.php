a42b8d8f0e8272d79feb25422a4665695e9fcc08
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-04-29 19:44:08 +0430

    Testing...

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(AdminsTableSeeder::class);
         $this->call(ProvincesTableSeeder::class);
         $this->call(CitiesTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(AttributeGroupsTableSeeder::class);
-        $this->call(AttributeValuesTableSeeder::class);
-        $this->call(PhotosTableSeeder::class);
-        $this->call(BrandsTableSeeder::class);
-        $this->call(ProductsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+//        $this->call(CategoriesTableSeeder::class);
+//        $this->call(AttributeGroupsTableSeeder::class);
+//        $this->call(AttributeValuesTableSeeder::class);
+//        $this->call(PhotosTableSeeder::class);
+//        $this->call(BrandsTableSeeder::class);
+//        $this->call(ProductsTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
     }
 }
