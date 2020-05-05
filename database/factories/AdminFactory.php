ef93ef29b7de3a7ec188b76e81d378237530ecaa
Baha2r <bahador.mirzazadeh98@gmail.com>
2020-05-05 13:59:29 +0430

    Default password changed

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -11,7 +11,7 @@ $factory->define(Admin::class, function (Faker $faker) {
         'last_name' => $faker->lastName,
         'email' => 'example@example.com',
         'phone_number' => '09197234460',
-        'password' => '$argon2id$v=19$m=1024,t=2,p=2$ZE5LSVBRcFRYdWJOQ0wwNg$AJlnH520hhpBfZm7df8Zk9fwI+TiRtfgbvaodqBeV6k', // password
+        'password' => '$argon2id$v=19$m=1024,t=2,p=2$cDJEcmFCYWlla2lJSGdqUQ$ZlINyWQpPYbxyJgx9DadkkCtH6SFHakMeSc2sRMkIQo', // password
         'super_admin' => 1,
     ];
 });
